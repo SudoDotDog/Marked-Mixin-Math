735c84f612e3579c59db9bd67bfb0d82bc3322e9
wmxpy <wm@mengw.io>
2022-09-18 01:45:11 -0500

    add math methods

--- src/methods.ts
+++ src/methods.ts
@@ -6,10 +6,54 @@
 
 export const MarkedMathMixinMethods: Record<string, any> = {
 
+    E: Math.E,
+    LN2: Math.LN2,
+    LN10: Math.LN10,
+    LOG2E: Math.LOG2E,
+    LOG10E: Math.LOG10E,
+    PI: Math.PI,
+
     abs: (value: number): number => {
         return Math.abs(value);
     },
+
+    log: (value: number): number => {
+        return Math.log(value);
+    },
+    log10: (value: number): number => {
+        return Math.log10(value);
+    },
+    log2: (value: number): number => {
+        return Math.log2(value);
+    },
+    log1p: (value: number): number => {
+        return Math.log1p(value);
+    },
+
+    ceil: (value: number): number => {
+        return Math.ceil(value);
+    },
     floor: (value: number): number => {
         return Math.floor(value);
+    },
+    round: (value: number): number => {
+        return Math.round(value);
+    },
+
+    sin: (value: number): number => {
+        return Math.sin(value);
+    },
+    cos: (value: number): number => {
+        return Math.cos(value);
+    },
+    tan: (value: number): number => {
+        return Math.tan(value);
+    },
+
+    pow: (value: number, power: number): number => {
+        return Math.pow(value, power);
+    },
+    sqrt: (value: number): number => {
+        return Math.sqrt(value);
     },
 };
